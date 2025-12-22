a8e40688db26e5e4200ec0a8148f981ce631368a
Alaricy79 <Alaricy@gmail.com>
2025-12-22 21:17:56 +0200

    test_pages_availabilit

--- news/tests/test_routes.py
+++ news/tests/test_routes.py
@@ -3,14 +3,33 @@ from http import HTTPStatus
 from django.test import TestCase
 # Импортируем функцию reverse().
 from django.urls import reverse
+from news.models import News
 
 
 class TestRoutes(TestCase):
 
-    def test_home_page(self):
-        # Вместо прямого указания адреса 
-        # получаем его при помощи функции reverse().
-        url = reverse('news:home')
-        response = self.client.get(url)
-        # Проверяем, что код ответа равен статусу OK (он же 200).
-        self.assertEqual(response.status_code, HTTPStatus.OK) 
+    @classmethod
+    def setUpTestData(cls):
+        cls.news = News.objects.create(title='Заголовок', text='Текст')
+
+    def test_pages_availability(self):
+        # Создаём набор тестовых данных - кортеж кортежей.
+        # Каждый вложенный кортеж содержит два элемента:
+        # имя пути и позиционные аргументы для функции reverse().
+        urls = (
+            ('news:home', None),
+            ('news:detail', (self.news.id,)),
+            ('users:login', None),
+            ('users:signup', None),
+        )
+        # Итерируемся по внешнему кортежу 
+        # и распаковываем содержимое вложенных кортежей:
+        for name, args in urls:
+            with self.subTest(name=name):
+                # Передаём имя и позиционный аргумент в reverse()
+                # и получаем адрес страницы для GET-запроса:
+                url = reverse(name, args=args)
+                response = self.client.get(url)
+                self.assertEqual(response.status_code, HTTPStatus.OK) 
+
+     
