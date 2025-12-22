3ce8bd8aef7710ec63b4680cdaf21ff8f3f25015
Roman V. Yudanov <Alaricy@gmail.com>
2025-12-22 16:59:49 +0200

    skip decorator

--- news/tests/test_trial.py
+++ news/tests/test_trial.py
@@ -1,13 +1,27 @@
 from django.test import TestCase
 
+from news.models import News
+from unittest import skip
 
-class Test(TestCase):
+@skip("хочу")
+class TestNews(TestCase):
+    # Все нужные переменные сохраняем в атрибуты класса.
+    TITLE = 'Заголовок новости'
+    TEXT = 'Тестовый текст'
 
-    def test_example_success(self):
-        self.assertTrue(True)
+    @classmethod
+    def setUpTestData(cls):
+        cls.news = News.objects.create(
+            # При создании объекта обращаемся к константам класса через cls.
+            title=cls.TITLE,
+            text=cls.TEXT,
+        )
 
+    def test_successful_creation(self):
+        news_count = News.objects.count()
+        self.assertEqual(news_count, 1)
 
-class YetAnotherTest(TestCase):
-
-    def test_example_success(self):
-        self.assertTrue(False)
+    def test_title(self):
+        # Чтобы проверить равенство с константой -
+        # обращаемся к ней через self, а не через cls:
+        self.assertEqual(self.news.title, self.TITLE)
