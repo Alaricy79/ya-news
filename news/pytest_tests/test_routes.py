d0b33a5d75ed40b2820cf86bd3a6f86352ce114b
Roman V. Yudanov <Alaricy@gmail.com>
2025-12-26 10:55:33 +0200

    anonymus ok

--- news/pytest_tests/test_routes.py
+++ news/pytest_tests/test_routes.py
@@ -1,16 +1,35 @@
 import pytest
+
 from http import HTTPStatus
 
 from django.urls import reverse
 
+from news.models import News
+
+
+@pytest.fixture
+def news_fixture():
+    some_news = News.objects.create(
+        title='Заголовок новости',
+        text='Текст новости',
+    )
+    return some_news
+
+
 @pytest.mark.django_db
-@pytest.mark.parametrize(
-    'name',  # Имя параметра функции.
-    # Значения, которые будут передаваться в name.
-    ('news:home', 'users:login', 'users:signup')
-)
-# Указываем имя изменяемого параметра в сигнатуре теста.
-def test_pages_availability_for_anonymous_user(client, name):
-    url = reverse(name)  # Получаем ссылку на нужный адрес.
-    response = client.get(url)  # Выполняем запрос.
+@pytest.mark.parametrize('url_name', (
+    'news:home',
+    'users:login',
+    'users:signup',
+))
+def test_pages_availability_anonymus(client, url_name):
+    url = reverse(url_name)
+    response = client.get(url)
+    assert response.status_code == HTTPStatus.OK
+
+
+@pytest.mark.django_db
+def test_news_detail_availability_anonymus(client, news_fixture):
+    url = reverse('news:detail', args=[news_fixture.id])
+    response = client.get(url)
     assert response.status_code == HTTPStatus.OK
